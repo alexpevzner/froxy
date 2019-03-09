2dc0b1c0a15c968e0bbe33d31e6eb1494a5bb913
Alexander Pevzner <pzz@apevzner.com>
2019-03-09 12:43:28 +0300

    Table of sites made semi-functional

--- src/pages/content/sites/_index.md
+++ src/pages/content/sites/_index.md
@@ -8,16 +8,17 @@ Here you can edit list of sites which will be accessed via server.
 In most cases you will want to check the "With subdomains" button
 
 <table >
-	<tbody>
+	<tbody id="tbody">
 	<tr>
 		<td><input id="add.host" type="text" style="width: 95%;"/></td>
 		<td>&nbsp;<input id="add.rec" type="checkbox" checked /> &nbsp;With subdomains
 		<td><input type="button" value="Add" onclick="AddSite()" /></td>
 	</tr>
-	<tr>
-		<td><input type="text" style="width: 95%;" disabled/></td>
-		<td>&nbsp;<input type="checkbox" checked disabled /> &nbsp;With subdomains
-		<td><input type="button" value="Del"/></td>
+	<tr id="template" hidden>
+		<td><input name="host" type="text" style="width: 95%;" /></td>
+		<td>&nbsp;<input name="rec" type="checkbox" checked /> &nbsp;With subdomains
+		<td><input name="update" type="button" value="Update"/></td>
+		<td><input name="del" type="button" value="Del"/></td>
 	</tr>
 	</tbody>
 </table>
--- src/pages/static/sites.js
+++ src/pages/static/sites.js
@@ -2,6 +2,12 @@
 // Site list page script
 //
 
+// ----- Static variables -----
+//
+// Array of table rows, one per site, grows or shrinks dynamically
+//
+var table = [];
+
 //
 // Add a site
 //
@@ -17,11 +23,54 @@ function AddSite () {
 }
 
 //
+// Resize table
+//
+function ResizeTable (sz) {
+    if (table.length > sz) {
+        while(table.length > sz) {
+            table.pop().remove();
+        }
+    } else {
+        var tbody = document.getElementById("tbody");
+
+        while(table.length < sz) {
+            var row = document.getElementById("template").cloneNode(true);
+            row.hidden = false;
+
+            inputs = row.getElementsByTagName("input");
+            for (var i = 0; i < inputs.length; i ++) {
+                var elm = inputs[i];
+                var nm = elm.getAttribute("name");
+                elm.id = table.length + "." + nm;
+            }
+
+            tbody.appendChild(row);
+            table.push(row);
+        }
+    }
+}
+
+//
+// Update table
+//
+function UpdateTable (sites) {
+    rq = tproxy.GetSites();
+
+    rq.OnSuccess = function (sites) {
+        ResizeTable(sites.length);
+        for (var row = 0; row < table.length; row ++) {
+            tproxy.UiSetInput(row + ".host", sites[row].host);
+        }
+    };
+}
+
+//
 // Page initialization
 //
 function init () {
+    UpdateTable();
 }
 
 init();
 
-// vim:ts=8:sw=2:et
+// vim:ts=8:sw=4:et
