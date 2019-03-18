f43c726c00d056ba5b6be3b967546f1d2fe18e3b
Alexander Pevzner <pzz@apevzner.com>
2019-03-18 21:14:42 +0300

    Added counter of direct TCP connections

--- src/cmd/tproxy/counters.go
+++ src/cmd/tproxy/counters.go
@@ -9,6 +9,7 @@ package main
 //
 type Counters struct {
 	Tag            uint64 `json:"tag"`
+	TCPConnections int32  `json:"tcp_conns"`    // Direct TCP connections
 	SSHSessions    int32  `json:"ssh_sessions"` // Count of SSH client sessions
 	SSHConnections int32  `json:"ssh_conns"`    // Count of connections via SSH
 }
--- src/cmd/tproxy/direct.go
+++ src/cmd/tproxy/direct.go
@@ -15,33 +15,77 @@ import (
 // Transport that connects directly
 //
 type DirectTransport struct {
-	http.Transport
-	env *Env // Back link to environment
+	http.Transport      // Direct http.Transport
+	env            *Env // Back link to environment
+}
+
+//
+// Direct TCP connection
+//
+type directConn struct {
+	net.Conn                   // Underlying net.Conn
+	transport *DirectTransport // Transport that owns the connection
 }
 
 //
 // Create new DirectTransport
 //
 func NewDirectTransport(env *Env) *DirectTransport {
-	return &DirectTransport{
+	t := &DirectTransport{
 		Transport: http.Transport{
-			Proxy: nil,
-			DialContext: (&net.Dialer{
-				Timeout:   10 * time.Second,
-				KeepAlive: 10 * time.Second,
-				DualStack: true,
-			}).DialContext,
+			Proxy:                 nil,
 			MaxIdleConns:          100,
 			IdleConnTimeout:       90 * time.Second,
 			TLSHandshakeTimeout:   10 * time.Second,
 			ExpectContinueTimeout: 1 * time.Second,
 		},
+		env: env,
 	}
+
+	t.Transport.DialContext = t.DialContext
+
+	return t
 }
 
 //
 // Dial new TCP connection
 //
-func (t *DirectTransport) Dial(net, addr string) (net.Conn, error) {
-	return t.DialContext(context.Background(), net, addr)
+func (t *DirectTransport) Dial(network, addr string) (net.Conn, error) {
+	return t.DialContext(context.Background(), network, addr)
+}
+
+//
+// Dial new TCP connection with context
+//
+func (t *DirectTransport) DialContext(ctx context.Context,
+	network, addr string) (net.Conn, error) {
+
+	dialer := &net.Dialer{
+		Timeout:   10 * time.Second,
+		KeepAlive: 10 * time.Second,
+		DualStack: true,
+	}
+
+	conn, err := dialer.DialContext(ctx, network, addr)
+	if err != nil {
+		return nil, err
+	}
+
+	t.env.IncCounter(&t.env.Counters.TCPConnections)
+
+	dirconn := &directConn{
+		Conn:      conn,
+		transport: t,
+	}
+
+	return dirconn, nil
+}
+
+//
+// Close directConn
+//
+func (c *directConn) Close() error {
+	t := c.transport
+	t.env.DecCounter(&t.env.Counters.TCPConnections)
+	return c.Conn.Close()
 }
