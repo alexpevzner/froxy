0ee06795cba632e9e025896a2af7ef3638e66770
Alexander Pevzner <pzz@apevzner.com>
2019-03-29 18:34:29 +0300

    tproxy -i works on Windows

--- src/cmd/tproxy/adm_windows.go
+++ src/cmd/tproxy/adm_windows.go
@@ -11,10 +11,13 @@ package main
 import "C"
 
 import (
-	"errors"
 	"os"
 	"os/exec"
+	"runtime"
 	"syscall"
+
+	"github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
 )
 
 //
@@ -22,7 +25,42 @@ import (
 //
 func (adm *Adm) CreateDesktopShortcut(outpath, comment, args string,
 	startup bool) error {
-	return errors.New("Not implemented")
+
+	// Obtain name of executable file
+	exe, err := os.Executable()
+	if err != nil {
+		return err
+	}
+
+	// Lock OS thread. Otherwise OLE will get crazy
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	// Do the OLE mess
+	ole.CoInitializeEx(0, ole.COINIT_APARTMENTTHREADED|ole.COINIT_SPEED_OVER_MEMORY)
+	oleShellObject, err := oleutil.CreateObject("WScript.Shell")
+	if err != nil {
+		return err
+	}
+	defer oleShellObject.Release()
+	wshell, err := oleShellObject.QueryInterface(ole.IID_IDispatch)
+	if err != nil {
+		return err
+	}
+	defer wshell.Release()
+	cs, err := oleutil.CallMethod(wshell, "CreateShortcut", outpath)
+	if err != nil {
+		return err
+	}
+	idispatch := cs.ToIDispatch()
+	oleutil.PutProperty(idispatch, "IconLocation", adm.Env.PathUserIconFile)
+	oleutil.PutProperty(idispatch, "TargetPath", exe)
+	oleutil.PutProperty(idispatch, "Arguments", args)
+	oleutil.PutProperty(idispatch, "Description", comment)
+	oleutil.PutProperty(idispatch, "WindowStyle", 7)
+	_, err = oleutil.CallMethod(idispatch, "Save")
+
+	return err
 }
 
 //
